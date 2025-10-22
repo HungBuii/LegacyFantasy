c827763e01a6e3ceb1dfec197965df743bc6de93
Hungg Duy Bui <buiduyhungg2310@gmail.com>
2025-10-22 10:53:49 +0700

    create "Idle, Run, Jump, Fall States Character"

---
+++ Source/LegacyFantasy/Character/SelectedCharacter.h
@@ -0,0 +1,58 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperZDCharacter.h"
+#include "SelectedCharacter.generated.h"
+
+
+struct FInputActionValue;
+
+
+UCLASS()
+class LEGACYFANTASY_API ASelectedCharacter : public APaperZDCharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ASelectedCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+	
+	/* Camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, meta=(AllowPrivateAccess))
+	class USpringArmComponent* SpringArm;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, meta=(AllowPrivateAccess))
+	class UCameraComponent* Camera;
+
+	/* Inputs */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta=(AllowPrivateAccess))
+	class UInputMappingContext* InputMappingContext;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta=(AllowPrivateAccess))
+	class UInputAction* MoveAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta=(AllowPrivateAccess))
+	class UInputAction* JumpAction;
+
+	/* Function */
+	void Move(const FInputActionValue& Value);
+	void JumpStarted(const FInputActionValue& Value);
+	void JumpEnded(const FInputActionValue& Value);
+
+	void UpdateDirection(float MoveDirection);
+};
